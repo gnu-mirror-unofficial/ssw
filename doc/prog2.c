f2ae2df9af840878dedaa3ad279ced80ae8f7006
John Darrington <john@darrington.wattle.id.au>
2017-09-12 11:08:06 +0200

    Documentation: Add a description about adding a model to the sheet

---
+++ doc/prog2.c
@@ -0,0 +1,42 @@
+#include <gtk/gtk.h>
+#include <ssw-sheet.h>
+
+GtkTreeModel *
+create_data_model (void)
+{
+  GtkListStore *model = gtk_list_store_new (3,
+					    G_TYPE_INT,
+					    G_TYPE_STRING,
+					    G_TYPE_DOUBLE);
+  for (int i = 0; i < 4; ++i)
+    {
+      GtkTreeIter iter;
+      gtk_list_store_append (model, &iter);
+      gtk_list_store_set (model, &iter,
+			  0, i,
+			  1, (i % 2) ? "odd" : "even",
+			  2, i / 3.0,
+			  -1);
+    }
+
+  return GTK_TREE_MODEL (model);
+}
+
+int
+main (int argc, char **argv)
+{
+  gtk_init (&argc, &argv);
+
+  GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  GtkWidget *sheet = ssw_sheet_new ();
+  
+  GtkTreeModel *data = create_data_model ();
+  g_object_set (sheet, "data-model", data, NULL);
+
+  gtk_container_add (GTK_CONTAINER (window), sheet);
+  gtk_window_maximize (GTK_WINDOW (window));
+  gtk_widget_show_all (window);
+  gtk_main ();
+
+  return 0;
+}
